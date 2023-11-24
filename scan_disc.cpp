e16963c9de56b18f092b94d4beed02319a643a13
SanathKulla <110275934+SanathKulla@users.noreply.github.com>
2023-11-24 15:33:37 +0530

    Create scan_disc.cpp

---
+++ scan_disc.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int head , n;
+    cin>>head>>n;
+    vector<int> v(n);
+        for(int i=0; i<n; i++){
+            cin>>v[i];
+    }
+    sort(v.begin() , v.end());
+    int head_moments = 0;
+    vector<int> sequence;
+    sequence.push_back(head);
+    //right
+    int r = n;
+    for(int i = n-1;i>=0;i--){
+        if(v[i]>=head)r = i;
+    }
+    int l = r-1;
+    while(r < n){
+       head_moments += abs(head - v[r]);
+       head = v[r];
+       sequence.push_back(head);
+       ++r;
+    }
+    head_moments += 199 - head;
+    head = 199;
+    while(l >= 0){
+       head_moments += abs(head - v[l]);
+       head = v[l];
+       sequence.push_back(head);
+       --l;  
+    }
+    cout<<head_moments<<endl;
+    for(auto it:sequence){
+        cout<<it<<" ";
+    }
+    cout<<endl;
+
+}
